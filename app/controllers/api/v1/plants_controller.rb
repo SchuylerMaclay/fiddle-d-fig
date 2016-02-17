c68dfa668f8ee3c0942a542f8bba27cec2daf16d
Skitelman <sghitelman@gmail.com>
2016-02-17 16:24:46 -0500

    added plant crud actions to api

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -2,4 +2,30 @@ class Api::V1::PlantsController < ApplicationController
   def index
     render json: Plant.all
   end
+
+  def show
+    render json: Plant.find(params[:id])
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant
+  end
+
+  def update
+    plant = Plant.find(params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    render nothing: true
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:name, :image_url, :light, :water, :description)
+  end
 end
