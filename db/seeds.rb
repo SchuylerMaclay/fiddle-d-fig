a4a9da21ab384807b35f541e92c9652c4a8414f5
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-17 16:05:55 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Plant.create(
+  name: "Fiddle Leaf Fig",
+  imgae_url: "http://www.littlefig.com.au/wp-content/uploads/2015/03/fiddle-leaf-fig-journey.jpg",
+  light: "bright consistent light",
+  water: "When its dry to the touch",
+  description: "The best Plant")
+
+  puts "seeded"
