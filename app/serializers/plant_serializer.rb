824e4f2710ce37157a422ea10be3b64e2ccaa71f
Skitelman <sghitelman@gmail.com>
2016-02-18 16:44:17 -0500

    created model and serializer

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,3 +1,5 @@
 class PlantSerializer < ActiveModel::Serializer
+  embed :ids
   attributes :id, :name, :image_url, :light, :water, :description
+  has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217213301) do
+ActiveRecord::Schema.define(version: 20160218211626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,4 +26,14 @@ ActiveRecord::Schema.define(version: 20160217213301) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "plant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["plant_id"], name: "index_reviews_on_plant_id", using: :btree
+
+  add_foreign_key "reviews", "plants"
 end
