4021d8ad459ff654640c6ce635e385f78b481e60
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-20 21:00:39 -0500

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_plants, except: [:new, :edit]
   post "/users/sign_in", to: "sessions#create"
   namespace :api do
     namespace :v1 do
+      resources :user_plants, except: [:new, :edit]
       resources :plants, except: [:new, :edit]
       resources :reviews, except: [:new, :edit]
     end
